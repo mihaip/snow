8721c1b5a0981d133063f7f00630333ea7e3bd51
Mihai Parparita <mihai@persistent.info>
2026-01-11 22:04:02 -0800

    Add emulator loop and framebuffer support to Emscripten port
    Mostly worked out of the box, but the Emscripten stack size had to be
    increased and we also needed to make sure that we drain the audio
    data receiver, otherwise the sender would block once it filled up.

---
+++ frontend_im/src/framebuffer.rs
@@ -0,0 +1,49 @@
+use crossbeam_channel::Receiver;
+use snow_core::renderer::DisplayBuffer;
+
+unsafe extern "C" {
+    fn js_did_open_video(width: u32, height: u32);
+    fn js_blit(buf_ptr: *const u8, buf_size: u32);
+}
+
+pub(crate) struct Sender {
+    receiver: Receiver<DisplayBuffer>,
+    current_width: u16,
+    current_height: u16,
+}
+
+impl Sender {
+    pub(crate) fn new(receiver: Receiver<DisplayBuffer>) -> Self {
+        Self {
+            receiver,
+            current_width: 0,
+            current_height: 0,
+        }
+    }
+
+    pub(crate) fn tick(&mut self) {
+        while let Ok(frame) = self.receiver.try_recv() {
+            self.send_frame(frame);
+        }
+    }
+
+    fn send_frame(&mut self, frame: DisplayBuffer) {
+        let width = frame.width();
+        let height = frame.height();
+
+        if width != self.current_width || height != self.current_height {
+            unsafe {
+                js_did_open_video(width as u32, height as u32);
+            }
+            self.current_width = width;
+            self.current_height = height;
+        }
+
+        let data = frame.into_inner();
+        if !data.is_empty() {
+            unsafe {
+                js_blit(data.as_ptr(), data.len() as u32);
+            }
+        }
+    }
+}
