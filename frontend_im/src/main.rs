f02d37476f20fb36c9209a7e92926ec7703efd67
Mihai Parparita <mihai@persistent.info>
2026-01-18 13:38:44 -0800

    Add JsDiskImage backend implementation for SCSI disks
    Allows loading them from the streamed/cached disk image setup that
    Infinite Mac has.

--- frontend_im/src/main.rs
+++ frontend_im/src/main.rs
@@ -1,8 +1,11 @@
+use disk::JsDiskImage;
 use snow_core::emulator::comm::EmulatorCommand;
 use snow_core::emulator::Emulator;
 use snow_core::mac::MacModel;
 use snow_core::tickable::Tickable;
+use std::path::Path;
 
+mod disk;
 mod framebuffer;
 
 fn main() {
@@ -15,6 +18,7 @@ fn main() {
     let rom_path: String = args
         .value_from_str("--bootrom")
         .unwrap_or_else(|_| "/rom".to_string());
+    let scsi_disk: Option<String> = args.opt_value_from_str("--scsi-disk").unwrap_or(None);
 
     let rom_data = std::fs::read(&rom_path).expect("Failed to read ROM");
 
@@ -22,6 +26,19 @@ fn main() {
     let (mut emulator, frame_receiver) =
         Emulator::new(&rom_data, &[], model).expect("Failed to create emulator");
     let audio_receiver = emulator.get_audio();
+
+    if let Some(disk_name) = scsi_disk {
+        match JsDiskImage::open(Path::new(&disk_name)) {
+            Ok(disk) => {
+                if let Err(err) = emulator.attach_disk_image_at(Box::new(disk), 0) {
+                    log::error!("Failed to attach SCSI disk '{}': {}", disk_name, err);
+                }
+            }
+            Err(err) => {
+                log::error!("Failed to open SCSI disk '{}': {}", disk_name, err);
+            }
+        }
+    }
 
     let cmd_sender = emulator.create_cmd_sender();
     cmd_sender.send(EmulatorCommand::Run).unwrap();
