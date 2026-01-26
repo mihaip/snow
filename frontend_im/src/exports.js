b13cbe5db062c0501f9c00f080f0fdb23424e092
Mihai Parparita <mihai@persistent.info>
2026-01-25 17:30:17 -0800

    Add support for audio in the Infinite Mac frontend
    Adds a new JsAudioSink implementation that sends audio data to the JavaScript
    side and had similar backpressure/pacing logic to run at realtime speeds.

--- frontend_im/src/exports.js
+++ frontend_im/src/exports.js
@@ -2,77 +2,97 @@
 // These functions are callable from Rust via extern "C"
 
 mergeInto(LibraryManager.library, {
-    js_did_open_video: function(width, height) {
+    // Internals
+    js_sleep(seconds) {
+        workerApi.sleep(seconds);
+    },
+
+    // Video
+    js_did_open_video(width, height) {
         workerApi.didOpenVideo(width, height);
     },
-    js_blit: function(bufPtr, bufSize) {
+    js_blit(bufPtr, bufSize) {
         workerApi.blit(bufPtr, bufSize);
     },
-    js_disk_open: function(ptr) {
+
+    // Audio
+    js_did_open_audio(sampleRate, sampleSize, channels) {
+        workerApi.didOpenAudio(sampleRate, sampleSize, channels);
+    },
+    js_audio_buffer_size() {
+        return workerApi.audioBufferSize();
+    },
+    js_enqueue_audio(bufPtr, bufSize) {
+        workerApi.enqueueAudio(bufPtr, bufSize);
+    },
+
+    // Disks
+    js_disk_open(ptr) {
         return workerApi.disks.open(UTF8ToString(ptr));
     },
-    js_disk_close: function(diskId) {
+    js_disk_close(diskId) {
         workerApi.disks.close(diskId);
     },
-    js_disk_size: function(diskId) {
+    js_disk_size(diskId) {
         return workerApi.disks.size(diskId);
     },
-    js_disk_read: function(diskId, bufPtr, offset, length) {
+    js_disk_read(diskId, bufPtr, offset, length) {
         return workerApi.disks.read(diskId, bufPtr, offset, length);
     },
-    js_disk_write: function(diskId, bufPtr, offset, length) {
+    js_disk_write(diskId, bufPtr, offset, length) {
         return workerApi.disks.write(diskId, bufPtr, offset, length);
     },
-    js_acquire_input_lock: function() {
+
+    // Input
+    js_acquire_input_lock() {
         return workerApi.acquireInputLock();
     },
-    js_release_input_lock: function() {
+    js_release_input_lock() {
         workerApi.releaseInputLock();
     },
-    js_has_mouse_position: function() {
+    js_has_mouse_position() {
         return workerApi.getInputValue(
             workerApi.InputBufferAddresses.mousePositionFlagAddr
         );
     },
-    js_get_mouse_x_position: function() {
+    js_get_mouse_x_position() {
         return workerApi.getInputValue(
             workerApi.InputBufferAddresses.mousePositionXAddr
         );
     },
-    js_get_mouse_y_position: function() {
+    js_get_mouse_y_position() {
         return workerApi.getInputValue(
             workerApi.InputBufferAddresses.mousePositionYAddr
         );
     },
-    js_get_mouse_delta_x: function() {
+    js_get_mouse_delta_x() {
         return workerApi.getInputValue(
             workerApi.InputBufferAddresses.mouseDeltaXAddr
         );
     },
-    js_get_mouse_delta_y: function() {
+    js_get_mouse_delta_y() {
         return workerApi.getInputValue(
             workerApi.InputBufferAddresses.mouseDeltaYAddr
         );
     },
-    js_get_mouse_button_state: function() {
+    js_get_mouse_button_state() {
         return workerApi.getInputValue(
             workerApi.InputBufferAddresses.mouseButtonStateAddr
         );
     },
-    js_has_key_event: function() {
+    js_has_key_event() {
         return workerApi.getInputValue(
             workerApi.InputBufferAddresses.keyEventFlagAddr
         );
     },
-    js_get_key_code: function() {
-        return workerApi.getInputValue(workerApi.InputBufferAddresses.keyCodeAddr);
+    js_get_key_code() {
+        return workerApi.getInputValue(
+            workerApi.InputBufferAddresses.keyCodeAddr
+        );
     },
-    js_get_key_state: function() {
+    js_get_key_state() {
         return workerApi.getInputValue(
             workerApi.InputBufferAddresses.keyStateAddr
         );
-    },
-    js_console_log: function(ptr) {
-        console.log(UTF8ToString(ptr));
     },
 });
