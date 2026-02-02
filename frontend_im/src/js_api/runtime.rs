faa0ab896e94b84209e8e42af4e6bf5c1f92d380
Mihai Parparita <mihai@persistent.info>
2026-02-01 16:53:27 -0800

    Add support for CD-ROM images in the Infinite Mac frontend
    Introduces a CdromManager which which initializes a CD-ROM device and
    handles both any initial CD-ROM images as well as request to mount
    additional ones from the JavaScript side.

--- frontend_im/src/js_api/runtime.rs
+++ frontend_im/src/js_api/runtime.rs
@@ -1,11 +1,15 @@
 extern "C" {
+    fn js_check_for_periodic_tasks();
     fn js_sleep(secs: f64);
 }
 
-pub fn sleep_seconds(secs: f64) {
-    if secs <= 0.0 {
-        return;
+pub fn check_for_periodic_tasks() {
+    unsafe {
+        js_check_for_periodic_tasks();
     }
+}
+
+pub fn sleep_seconds(secs: f64) {
     unsafe {
         js_sleep(secs);
     }
